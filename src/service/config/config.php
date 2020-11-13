2c9931bfdaf2ff0fe3c4ac5c018cbb6cc3e5dd97
Rob Zeeman <this_is_not_an_email-3cc036843bde09c86580da2d3d753a527d1e8bfa@authentication.clariah.nl>
2020-11-13 16:02:51 +0100

    Proper service settings

--- src/service/config/config.php
+++ src/service/config/config.php
@@ -1,6 +1,6 @@
 <?php
-define('BASE_URL', 'http://www.huc.localhost');
+define('BASE_URL', 'https://isidore.sd.di.huc.knaw.nl');
 define('APPATH', dirname(dirname(__FILE__)));
 define("PAGE_LENGTH", 20);
 define ("BROWSE_PAGE_LENGTH", 500);
-define("ELASTIC_HOST", "http://localhost:9200/manuscripts/_search?");
+define("ELASTIC_HOST", "http://elastic:9200/manuscripts/_search?");
